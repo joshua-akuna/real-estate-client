0cc5da31d18841a31878e8f70c796825b8adddf7
Akuna Joshua <akunajoshua@gmail.com>
2026-02-15 22:53:27 +0100

    login page implemented

---
+++ src/app/auth/login/page.jsx
@@ -0,0 +1,9 @@
+import LoginForm from '@/components/forms/LoginForm';
+
+export const metadata = {
+  title: 'Login - Real Estate App',
+};
+
+export default function LoginPage() {
+  return <LoginForm />;
+}
