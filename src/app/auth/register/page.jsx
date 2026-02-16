004b24c02a6fffdb65bc3cbce24501ea40d2d592
Akuna Joshua <akunajoshua@gmail.com>
2026-02-16 13:32:09 +0100

    register page created

---
+++ src/app/auth/register/page.jsx
@@ -0,0 +1,9 @@
+import RegisterForm from '@/components/forms/RegisterForm';
+
+export const metadata = {
+  title: 'Register | Real Estate App',
+};
+
+export default function RegisterPage() {
+  return <RegisterForm />;
+}
